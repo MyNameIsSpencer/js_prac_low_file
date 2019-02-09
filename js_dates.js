68ae2858f1f54218dee565cec7e924f1203af9d4
spencer kyonka <spencer.kyonka@hotmail.ca>
2019-02-09 15:58:46 -0500

    add threesor and work on dates

--- js_dates.js
+++ js_dates.js
@@ -1,7 +1,36 @@
 
-var dater = new Date();
-var millis = dater.getMilliseconds();
+console.log("Units");
+console.log('------------');
+console.log(new Date());
+console.log(new Date().getMilliseconds());
+console.log(new Date().getSeconds());
+console.log(new Date().getMinutes());
+console.log(new Date().getDay());
+console.log(new Date().getMonth());
+console.log(new Date().getYear());
 
-
-console.log(dater);
-console.log(millis);
+console.log();
+console.log("Date Methods");
+console.log('------------');
+let dater = new Date();
+console.log(dater.getDate());
+console.log(dater.getFullYear());
+console.log(dater.getHours());
+console.log(dater.getTime());
+console.log(dater.getTimezoneOffset(), 'minutes');
+console.log('VVV Day of the month');
+console.log(dater.getUTCDate());
+console.log(dater.getUTCFullYear());
+console.log(dater.getUTCMilliseconds());
+console.log(Date.now());
+console.log('dater difference VVV');
+console.log(Date.now() - dater);
+console.log(Date.parse(new Date()));
+console.log(dater.setDate(22));
+console.log(dater.toDateString());
+console.log(dater.toGMTString());
+// console.log(dater.ISOString());
+console.log(dater.toJSON());
+console.log(dater.toLocaleDateString());
+console.log(dater.toLocaleString());
+console.log(dater.valueOf());
